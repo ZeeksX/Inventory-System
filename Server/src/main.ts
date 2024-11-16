1685b1fa4ba294e0b5bf576b695b604bf3c9e917
otunaboEmmanuel <162343881+otunaboEmmanuel@users.noreply.github.com>
2024-11-16 23:35:51 +0100

    UI changes

--- Server/src/main.ts
+++ Server/src/main.ts
@@ -5,9 +5,9 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = 3006;
+  const port = 3000;
   app.setGlobalPrefix('api/v1');
-  app.enableCors({ origin: 'http://localhost:5174' });
+  app.enableCors({ origin: 'http://localhost:5173' });
 
   app.useGlobalPipes(
     new ValidationPipe({
